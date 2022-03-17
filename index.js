977f3cbc0e2166e0270ea2c96e8cba9a7bfd6d75
caitory <dev@caitory>
2022-03-17 21:33:07 +0900

    master

--- index.js
+++ index.js
@@ -12,14 +12,23 @@ const server = app.listen(3000, () =>
 app.use('/static', express.static(__dirname + '/resource'));
 
 const multer = require('multer')
-const upload = multer({dest: __dirname + '/resource'})
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, __dirname + '/resource/')
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + file.originalname)
+    }
+})
+const upload = multer({ storage: storage })
 
 app.post('/upload', upload.single('file'), (req, res, next) => 
 {
-    const {destination, filename, path} = req.file;
+    const {destination, filename, path, file, originalname} = req.file;
     console.log("파일 이름 : " + filename);
     console.log("파일 경로 : " + destination);
     console.log("파일 전체경로 : " + path);
-
+    //res.download("/file-api/resource/" + filename)
     res.send("http://workspace.caitory.com:3000/static/" + filename)
 })
